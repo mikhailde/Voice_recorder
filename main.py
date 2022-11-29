20a82ab89c7c47bcba4a40b49df1de80ac55d1ff
GoldenEagle74 <mshchannikov@yandex.ru>
2022-11-29 15:05:04 +0300

    Скрипт Python

---
+++ main.py
@@ -0,0 +1,12 @@
+import eel
+
+eel.init('web', allowed_extensions=['.js', '.html'])
+
+@eel.expose                     
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!') 
+
+eel.start('hello.html')
