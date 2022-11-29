0887b6df7f936be7fde4532838e8812555d359b1
GoldenEagle74 <mshchannikov@yandex.ru>
2022-11-29 20:05:51 +0300

    first commit

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ eel.init('web', allowed_extensions=['.js', '.html'])
 
 @eel.expose                     
 def say_hello_py(x):
-    print('Hello from %s' % x)
-
-say_hello_py('Python World!')
-eel.say_hello_js('Python World!') 
+    print('Hello from %s' % x) 
 
 eel.start('hello.html')
