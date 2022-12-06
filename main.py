4ec3da52515e436494f98bf1761462eea5a6e2db
GoldenEagle74 <mshchannikov@yandex.ru>
2022-12-06 22:11:04 +0300

    Добавлен класс для запуска функций в разных потоках

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import eel
+import web.backend.record as record
+import threading
 
-eel.init('web', allowed_extensions=['.js', '.html'])
 
-@eel.expose                     
+eel.init('web/frontend', allowed_extensions=['.js', '.html'])
+
+
+class MyThread(threading.Thread):
+    def run(self):
+        record.recording()
+        pass
+
+
+@eel.expose
 def say_hello_py(x):
-    print('Hello from %s' % x) 
+    print('Hello from %s' % x)
+
+
+@eel.expose
+def recording():
+    thread = MyThread()
+    thread.daemon = True
+    thread.start()
+
 
 eel.start('hello.html')
