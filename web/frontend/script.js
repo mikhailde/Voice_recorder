54fc1b8d3f74c36ff266a2205a1b3d6e0770ded8
GoldenEagle74 <mshchannikov@yandex.ru>
2022-12-15 15:08:58 +0300

    Перенес js в scrpt.js

--- web/frontend/script.js
+++ web/frontend/script.js
@@ -2,8 +2,8 @@ let input = document.querySelector('input');
 let form = document.querySelector('.u-form-spacing-15');
 form.onsubmit = function(evt) {
     evt.preventDefault();
-    eel.print(input.value);
-    // eel.recording(); // Call a Python function
-	// setTimeout(() => eel.recording(), 10000);
-  };
-  
+    eel.username(input.value);
+    window.location = "http://localhost:8000/index.html";
+  //   eel.recording(); // Call a Python function
+  // setTimeout(() => eel.recording(), 10000);
+};
